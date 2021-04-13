1028af191e56a2620290940e8c31449831c6496c
Alberto-Privado <alberto.diciembre.29@gmail.com>
2021-04-13 15:25:04 +0200

    (feat) Creating script to import products to Magento (in progress)
    - Adding data to CSV as needed. Some things are broken, working on fixing them
    
    refs: #gx4m01

--- .utils/csvImport.php
+++ .utils/csvImport.php
@@ -1,15 +1,52 @@
 <?php
-$fila = 2;
-if (($gestor = fopen("Sin título 2.csv", "r")) !== FALSE) {
+if (($file = fopen("Productos.csv", "r")) !== FALSE) {
     echo "Executing script...."."\n";
 
     $new = fopen('prueba.csv', 'w');
 
-    $header = array('sku,store_view_code,attribute_set_code,product_type,categories,product_websites,name,description,short_description,weight,product_online,tax_class_name,visibility,price,special_price,special_price_from_date,special_price_to_date,url_key,meta_title,meta_keywords,meta_description,created_at,updated_at,new_from_date,new_to_date,display_product_options_in,map_price,msrp_price,map_enabled,gift_message_available,custom_design,custom_design_from,custom_design_to,custom_layout_update,page_layout,product_options_container,msrp_display_actual_price_type,country_of_manufacture,additional_attributes,qty,out_of_stock_qty,use_config_min_qty,is_qty_decimal,allow_backorders,use_config_backorders,min_cart_qty,use_config_min_sale_qty,max_cart_qty,use_config_max_sale_qty,is_in_stock,notify_on_stock_below,use_config_notify_stock_qty,manage_stock,use_config_manage_stock,use_config_qty_increments,qty_increments,use_config_enable_qty_inc,enable_qty_increments,is_decimal_divided,website_id,deferred_stock_update,use_config_deferred_stock_update,related_skus,crosssell_skus,upsell_skus,hide_from_product_page,custom_options,bundle_price_type,bundle_sku_type,bundle_price_view,bundle_weight_type,bundle_values,associated_skus');
+    $header = array('sku,store_view_code,product_type,categories,name,description,weight,product_online,price,special_price,url_key,meta_keywords,meta_description,additional_attributes,qty,out_of_stock_qty,is_in_stock,related_skus,configurable_variations,configurable_variation_labels');
     fputs($new, implode(',',$header)."\n"); // Añadimos todas las columnas al nuevo archivo
 
-    while(($datos = fgetcsv($gestor, 1000, ",")) !== FALSE){
-        echo $datos[0];
+    $count = 0; // Counter what we will use to skip the first line always
+    while(($data = fgetcsv($file, 5000, ",")) !== FALSE){
+//        for($line = 0; $line < count($data); $line++){
+            $count++;
+            if ($count == 1) {
+                continue; // If the counter equals 1 that means it's the first line, so we skip it
+            }
+            $sku = $data[1];
+            $attributeSetCode = $data[0];
+            $productType = 'simple';
+            $categories = 'Default Category/' . $data[0];
+            $name = '"'. $data[30] . '"';
+            $description = '"'. $data[33]. '"';
+            $weight = $data[25];
+            $productOnline = $data[3];
+            $price = $data[7];
+            $specialPrice = $data[6];
+            $urlKey = $data[32];
+            $metaKeywords = '"' . $data[35] . '"';
+            $metaDescription = '"' . $data[34] . '"';
+            if($data[0] === 'Llantas'){
+//                $additionalAttributes = 'size='. $data[65] . '|color=' . $data[66] . '|mounting=' . $data[67] . '|diameter=' . $data[68];
+                $additionalAttributes = 'Placeholder';
+            } elseif ($data[0] === 'Neumaticos') {
+                $additionalAttributes = 'Placeholder 2';
+            } elseif ($data[0] === 'Kit muelles') {
+                $additionalAttributes = 'Placeholder 3';
+            } elseif ($data[0] === 'Suspensiones') {
+                $additionalAttributes = 'Placeholder 4';
+            } elseif ($data[0] === 'Separadores') {
+                $additionalAttributes = 'Placeholder 5';
+            }
+            $qty = $data[21];
+            $outOfStockQty = $data[22];
+            $isInStock = $data[40];
+            $product = $sku . ',' . $attributeSetCode. ',' . $productType . ',' . $categories . ',' . $name . ',' . $description . ',' . $weight .
+                ',' . $productOnline . ',' . $price . ',' . $specialPrice . ',' . $urlKey . ',' . $metaKeywords . ',' . $metaDescription .
+                ',' . $additionalAttributes . ',' . $qty . ',' . $outOfStockQty . ',' . $isInStock . "\n";
+            fputs($new, $product);
+//        }
     }
     echo "Finished execution."."\n";
 }
