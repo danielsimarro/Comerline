cef496626c5be08cff0fe6e54cfc0a1ca62ac84f
Alberto-Privado <alberto.diciembre.29@gmail.com>
2021-04-16 15:25:26 +0200

    (feat) Creating script to import products to Magento
    - Created the CSV with the info we need. Still needs some tweaking because Magento does some things wrong (specially with configurable products, sometimes it screws the import and sometimes don't), but it's almost the one we want
    
    refs: #gx4m01

--- .utils/csvImport.php
+++ .utils/csvImport.php
@@ -1,6 +1,6 @@
 <?php
-if (($file = fopen("Productos.csv", "r")) !== FALSE) {
-    echo "Executing script...."."\n";
+if (($file = fopen("Productos Test.csv", "r")) !== false) {
+    echo "Executing script...." . "\n";
 
     $new = fopen('prueba.csv', 'w');
 
@@ -21,13 +21,10 @@ if (($file = fopen("Productos.csv", "r")) !== FALSE) {
         'qty',
         'out_of_stock_qty',
         'is_in_stock',
-        'related_skus',
         'configurable_variations',
-        'configurable_variation_labels'
     ];
     fputcsv($new, $header, ";"); // We add the header to the new CSV
     $count = 0; // Counter what we will use to skip the first line always
-    $products = [];
     while (($data = fgetcsv($file, 5000, ";", "\"")) !== false) {
         $count++;
         if ($count == 1) {
@@ -38,13 +35,13 @@ if (($file = fopen("Productos.csv", "r")) !== FALSE) {
         $product['attribute_set_code'] = $data[0];
         $product['product_type'] = 'simple';
         $product['categories'] = 'Default Category/' . $data[0];
-        $product['name'] = $data[33] . '"';
+        $product['name'] = $data[33];
         $product['description'] = $data[39];
         $product['weight'] = $data[28];
         $product['product_online'] = $data[3];
         $product['price'] = $data[7];
         $product['special_price'] = $data[6];
-        $product['url_key'] = $data[37];
+        $product['url_key'] = $data[37] . "-" . $count;
         $product['meta_keywords'] = $data[43];
         if ($data[0] === 'Llantas') {
             $product['additional_attributes'] = 'code=' . $data[23] . '|size=' . $data[74] . '|color=' . $data[75] . '|mounting=' . $data[76] . '|diameter=' . $data[77];
@@ -90,7 +87,7 @@ if (($file = fopen("Productos.csv", "r")) !== FALSE) {
 
     // Now with the code and the SKU, for each product we have saved on the array, we have to change
     // it's product type to configurable and add to the configurable variations the SKUs
-
+    $productsUpdate = [];
     foreach ($groups as $group) {
         if ($group[0] === '2;sku=001,code=|size=|color=|mounting=|diameter=' || $group[0] === '1;sku=sku,additional_attributes') {
             echo "Skipping line..." . "\n"; // We skip the first two lines because they are not correct
@@ -104,25 +101,58 @@ if (($file = fopen("Productos.csv", "r")) !== FALSE) {
                 $variations .= $explode[1] . "|"; // Adding the variations to the same variable to use it later
             }
 
-            $new2 = fopen('prueba2.csv', 'w'); // We create a new CSV with the info that we will be using
             $handle = fopen("prueba.csv", "r"); // We open the last CSV we created
             $lineNum = 1;
             while (($column = fgetcsv($handle, 5000, ";")) !== false) {
                 if ($lineNum !== intval($lineProduct)) {
-                    $lineNum++;
-                    fputcsv($new2, $column, ";"); // If the line is not the one we want to modify and we haven't modified it yet, we leave it as it is
+                    echo "Skipping line " . $lineNum . "...\n";
+                    $lineNum++; // If the line is not the one we want to modify, we skip it
                 } else {
-                    $column[2] = 'configurable';
-                    $column[17] = substr($variations, 0, -1);
-                    fputcsv($new2, $column, ";"); // If the line is the one we want, we modify it and add it to the CSV
+                    $updated = [];
+                    $updated['sku'] = $column[0];
+                    $updated['attribute_set_code'] = $column[1];
+                    $updated['product_type'] =  'configurable';
+                    $updated['categories'] = $column[3];
+                    $updated['name'] = $column[4];
+                    $updated['description'] = $column[5];
+                    $updated['weight'] = $column[6];
+                    $updated['product_online'] = $column[7];
+                    $updated['price'] = $column[8];
+                    $updated['special_price'] = $column[9];
+                    $updated['url_key'] = $column[10];
+                    $updated['meta_keywords'] = $column[11];
+                    $updated['additional_attributes'] = $column[12];
+                    $updated['qty'] = $column[13];
+                    $updated['out_of_stock_qty'] = $column[14];
+                    $updated['is_in_stock'] = $column[15];
+                    $updated['configurable_variations'] = substr($variations, 0, -1);
+                    if ($updated) { // If the product is not empty, we add it to the CSV
+                        $productsUpdate[$lineNum] = $updated;
+                    }
                     echo "Line " . $lineNum . " modified succesfully!\n";
                     $lineNum++;
                 }
-
             }
             fclose($handle);
         }
     }
+
+    $handle = fopen("prueba.csv", "r+"); // We open the last CSV we created
+    $updatedCSV = fopen('prueba2.csv', 'w'); // We create a new CSV with the info that we will update
+    $lineNum = 1;
+    while (($column = fgetcsv($handle, 5000, ";")) !== false) {
+        if ($productsUpdate[$lineNum]) {
+            $column[2] = $productsUpdate[$lineNum]['product_type'];
+            $column[16] = $productsUpdate[$lineNum]['configurable_variations'];
+            fputcsv($updatedCSV, $column, ";");
+            $lineNum++;
+        } else {
+            fputcsv($updatedCSV, $column, ";");
+            $lineNum++;
+        }
+    }
+    fclose($updatedCSV);
+    fclose($handle);
 
     echo "Finished execution." . "\n";
 }
