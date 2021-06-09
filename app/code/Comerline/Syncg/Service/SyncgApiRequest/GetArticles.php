21db686fca060b33c45191b0f7ed01bf34a6a37b
Alberto-Privado <alberto.diciembre.29@gmail.com>
2021-06-09 15:51:24 +0200

    (feat) Product Sync (in progress)
    - Created Attribute Set in case it doesn't exists in Magento 2
    - If the product exists, we update it. If not, we create it
    - Cleaned some useless code and done some tweaking
    
    refs: #m16ey2

--- app/code/Comerline/Syncg/Service/SyncgApiRequest/GetArticles.php
+++ app/code/Comerline/Syncg/Service/SyncgApiRequest/GetArticles.php
@@ -7,10 +7,12 @@ use Comerline\Syncg\Service\SyncgApiService;
 use Comerline\Syncg\Model\SyncgStatus;
 use Comerline\Syncg\Model\ResourceModel\SyncgStatus\CollectionFactory;
 use Comerline\Syncg\Model\ResourceModel\SyncgStatusRepository;
-use Comerline\Syncg\Model\SyncgStatusFactory;
-use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as ProductCollectionFactory;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Model\ResourceModel\Product as ProductResource;
 use GuzzleHttp\ClientFactory;
 use GuzzleHttp\Psr7\ResponseFactory;
+use Magento\Catalog\Api\Data\ProductInterfaceFactory as ProductFactory;
+use Magento\Eav\Model\Entity\Attribute\SetFactory as AttributeSetFactory;
 use Magento\Framework\Serialize\Serializer\Json;
 use Magento\Framework\Webapi\Rest\Request;
 use Psr\Log\LoggerInterface;
@@ -25,17 +27,45 @@ class GetArticles extends SyncgApiService
      */
     protected $config;
 
+    /**
+     * @var SyncgStatus
+     */
     private $syncgStatus;
 
+    /**
+     * @var CollectionFactory
+     */
     private $syncgStatusCollectionFactory;
 
-    private $syncgStatusFactory;
-
+    /**
+     * @var SyncgStatusRepository
+     */
     private $syncgStatusRepository;
 
+    /**
+     * @var
+     */
     private $order;
 
-    private $productCollectionFactory;
+    /**
+     * @var ProductFactory
+     */
+    private $productFactory;
+
+    /**
+     * @var ProductRepositoryInterface
+     */
+    private $productRepository;
+
+    /**
+     * @var ProductResource
+     */
+    private $productResource;
+
+    /**
+     * @var AttributeSetFactory
+     */
+    private $attributeSetFactory;
 
     public function __construct(
         Config $configHelper,
@@ -45,16 +75,20 @@ class GetArticles extends SyncgApiService
         LoggerInterface $logger,
         SyncgStatus $syncgStatus,
         CollectionFactory $syncgStatusCollectionFactory,
-        SyncgStatusFactory $syncgStatusFactory,
         SyncgStatusRepository $syncgStatusRepository,
-        ProductCollectionFactory $productCollectionFactory
+        ProductFactory $productFactory,
+        ProductRepositoryInterface $productRepository,
+        ProductResource $productResource,
+        AttributeSetFactory $attributeSetFactory
     ) {
         $this->config = $configHelper;
         $this->syncgStatus = $syncgStatus;
         $this->syncgStatusCollectionFactory = $syncgStatusCollectionFactory;
-        $this->syncgStatusFactory = $syncgStatusFactory;
         $this->syncgStatusRepository = $syncgStatusRepository;
-        $this->productCollectionFactory = $productCollectionFactory;
+        $this->productFactory = $productFactory;
+        $this->productRepository = $productRepository;
+        $this->productResource = $productResource;
+        $this->attributeSetFactory = $attributeSetFactory;
         parent::__construct($configHelper, $json, $responseFactory, $clientFactory, $logger);
     }
 
@@ -65,7 +99,7 @@ class GetArticles extends SyncgApiService
             'filtro' => json_encode(array(
                 "inicio" => $start,
                 "filtro" => array(
-                    array("campo" => "descripcion", "valor" => "BLACK MIRROR32", "tipo" => 0)
+                    array("campo" => "descripcion", "valor" => "BLACK DIAMOND", "tipo" => 0)
                 )
             )),
             'orden' => json_encode(array("campo" => "id", "orden" => "ASC"))
@@ -98,49 +132,71 @@ class GetArticles extends SyncgApiService
         }
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance(); // Instance of Object Manager. This is temporal, and will change in the near future
         if ($pages) {
-//            $collectionSyncg = $this->syncgStatusCollectionFactory->create();
-            $count = 0;
             foreach ($pages as $page){
                 for ($i = 0; $i < count($page); $i++){
-//                    $count++;
-                    $productCollection = $this->productCollectionFactory->create()
-                        ->addAttributeToFilter('sku', $page[$i]['ref_fabricante']);// We check if the SKU exists and, as a result, the product
-                        foreach ($productCollection as $ps) {
-                            if ($ps) {
-                                $ps->setSku($page[$i]['ref_fabricante']);
-                                $ps->setName($page[$i]['descripcion']);
-                                $ps->setAttributeSet($page[$i]['ref_fabricante']);
-                                if ($page[$i]['si_vender_en_web'] === true) {
-                                    $ps->setStatus(1);
-                                } else {
-                                    $ps->setStatus(0);
-                                }
-                                $ps->setTaxClassId(0);
-                                $ps->setTypeId('simple');
-                                $ps->setPrice($page[$i]['pvp1']);
-                                $ps->save();
-                            } else {
-                                $product = $objectManager->create('\Magento\Catalog\Model\Product');
-                                $product->setSku($page[$i]['ref_fabricante']);
-                                $product->setName($page[$i]['descripcion']);
-                                $product->setAttributeSet($page[$i]['ref_fabricante']);
-                                if ($page[$i]['si_vender_en_web'] === true) {
-                                    $product->setStatus(1);
-                                } else {
-                                    $product->setStatus(0);
-                                }
-                                $product->setTaxClassId(0);
-                                $product->setTypeId('simple');
-                                $product->setPrice($page[$i]['pvp1']);
-                                $product->save();
-                            }
+                    $attributeSetId = null;  // Variable where we will store the attribute set ID
+                    $collectionSyncg = $this->syncgStatusCollectionFactory->create()
+                        ->addFieldToFilter('g_id', $page[$i]['cod']); // We check if the product already exists
+                    $attributeSetCollectionFactory = $objectManager->get('Magento\Eav\Model\ResourceModel\Entity\Attribute\Set\CollectionFactory'); // Temporary use of object manager, this should be a Dependency Injection
+                    $attributeSetCollection = $attributeSetCollectionFactory->create();
+                    $attributeSets = $attributeSetCollection->getItems();
+                    foreach ($attributeSets as $attributeSet) {
+                        if ($attributeSet->getAttributeSetName() === $page[$i]['familias'][0]['nombre']) { // If the name of the attribute set is the same as the one on G4100...
+                             $attributeSetId = $attributeSet->getAttributeSetId(); // We save the ID to use it later
                         }
-//                    $name = $page[$i]['descripcion'];
-//                    $price = $page[$i]['pvp1'];
-//                    $code = $page[$i]['cod'];
-//                    $gId = $page[$i]['id'];
-//                    $model = $page[$i]['modelo'];
-//                    $this->syncgStatusRepository->updateEntityStatus($count, $gId, SyncgStatus::TYPE_PRODUCT, SyncgStatus::STATUS_COMPLETED); // Count used as a temporal solution, not final
+                    }
+                    if ($attributeSetId === null) { // If at this point $attributeSetId remains as null, it means we have to create the attribute set
+                        $createAttributeSet = $this->attributeSetFactory->create();
+                        $entityType = $objectManager->create('Magento\Eav\Model\Entity\Type')->loadByCode('catalog_product');
+                        $defaultSetId = $objectManager->create('Magento\Catalog\Model\Product')->getDefaultAttributeSetid();
+                        $data = [ // Data that we will use to create the Attribute Set
+                            'attribute_set_name' => $page[$i]['familias'][0]['nombre'],
+                            'entity_type_id' => $entityType->getId(),
+                            'sort_order' => 200,
+                        ];
+                        $createAttributeSet->setData($data);
+                        $createAttributeSet->validate(); // We need to validate it, or else the Attribute Set will be created but not available to select
+                        $createAttributeSet->save();
+                        $createAttributeSet->initFromSkeleton($defaultSetId); // We initialize the Attribute Set to make it visible on the frontend
+                        $createAttributeSet->save();
+                        $attributeSetId = $createAttributeSet->getAttributeSetId(); // We save the ID to use it later
+                    }
+                    if ($collectionSyncg->getSize() > 0) {
+                        foreach ($collectionSyncg as $itemSyncg) {
+                            $product_id = $itemSyncg->getData('mg_id');
+                            $product = $this->productRepository->getById($product_id, true);
+                            $product->setStoreId(0);
+                            $product->setTaxClassId(0);
+                            $product->setAttributeSetId($attributeSetId);
+                            $product->setTypeId('simple');
+                            $product->setPrice(188);
+                            $product->setSku($page[$i]['ref_fabricante']);
+                            $product->setAttributeSet($attributeSetId);
+                            $product->setName($page[$i]['descripcion']);
+                            if ($page[$i]['si_vender_en_web'] === true) {
+                                $product->setStatus(1);
+                            } else {
+                                $product->setStatus(0);
+                            }
+                            $this->productResource->save($product);
+                        }
+                    } else {
+                        $product = $this->productFactory->create();
+                        $product->setStoreId(0);
+                        $product->setTaxClassId(0);
+                        $product->setAttributeSetId($attributeSetId);
+                        $product->setTypeId('simple');
+                        $product->setPrice($page[$i]['pvp1']);
+                        $product->setSku($page[$i]['ref_fabricante']);
+                        $product->setName($page[$i]['descripcion']);
+                        if ($page[$i]['si_vender_en_web'] === true) {
+                            $product->setStatus(1);
+                        } else {
+                            $product->setStatus(0);
+                        }
+                        $product->save();
+                    }
+                        $this->syncgStatus = $this->syncgStatusRepository->updateEntityStatus($product->getEntityId(), $page[$i]['cod'], SyncgStatus::TYPE_PRODUCT, 1);
                 }
             }
         }
