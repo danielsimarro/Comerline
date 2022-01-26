cd7d8ba99d8752f7427d6ad2c0fc92c54553b3d0
Alberto-Privado <alberto.diciembre.29@gmail.com>
2022-01-26 12:18:47 +0100

    (feat) Config and Touch-ups in Product page - Added a condition to minimum quantity
    - Now, if the category is not 'Rims', the quantity will stay only 1
    
    refs: #1vj5awd

--- app/code/Comerline/ImprovedProduct/view/frontend/web/js/minimumQty.js
+++ app/code/Comerline/ImprovedProduct/view/frontend/web/js/minimumQty.js
@@ -2,7 +2,7 @@ require(["jquery"],
     function($){
         let breadcrumbs = $('.breadcrumbs').children().find('li');
         breadcrumbs.each(function (i) {
-            if ($(this).text().includes('Llantas')) {
+            if (($(this).text().includes('Llantas')) || ($(this).text().includes('Neumáticos'))) {
                 $('#qty').val('4');
             }
         });
