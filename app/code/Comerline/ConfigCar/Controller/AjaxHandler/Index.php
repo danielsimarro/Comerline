7c944300b2ba2c19e459faff72dbad4fe222816a
Alberto-Privado <alberto.diciembre.29@gmail.com>
2022-02-03 18:42:46 +0100

    (feat) Config Car module - Implementing changes requested on PR
    - Fixed error caused when trying to use template comparable-options.phtml
    - Deleted duplicated code
    - Tested and working
    
    refs: #1vj5w3r

--- app/code/Comerline/ConfigCar/Controller/AjaxHandler/Index.php
+++ app/code/Comerline/ConfigCar/Controller/AjaxHandler/Index.php
@@ -6,24 +6,26 @@ use Comerline\ConfigCar\Block\ConfigCar;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\Result\JsonFactory;
 use Magento\Directory\Model\RegionFactory;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\App\Action\Action;
 
-class Index extends \Magento\Framework\App\Action\Action
+class Index extends Action
 {
     protected $resultJsonFactory;
 
-    protected $regionColFactory;
+    protected $resultPageFactory;
 
-    private $configCar;
+    protected $regionColFactory;
 
     public function __construct(
         Context       $context,
         JsonFactory   $resultJsonFactory,
         RegionFactory $regionColFactory,
-        ConfigCar     $configCar
+        PageFactory   $resultPageFactory
     )
     {
-        $this->configCar = $configCar;
         $this->regionColFactory = $regionColFactory;
+        $this->resultPageFactory = $resultPageFactory;
         $this->resultJsonFactory = $resultJsonFactory;
         parent::__construct($context);
     }
@@ -31,18 +33,17 @@ class Index extends \Magento\Framework\App\Action\Action
     public function execute()
     {
         $result = $this->resultJsonFactory->create();
-
-        $html = '<option selected="selected" value="">Seleccione una opción</option>';
-
+        $resultPage = $this->resultPageFactory->create();
         $categoryId = $this->getRequest()->getParam('frame');
-        if ($categoryId != '') {
-            $category = $this->configCar->getCategoryById($categoryId);
-            $childrenCategories = $category->getChildrenCategories();
-            foreach ($childrenCategories as $children) {
-                $html .= '<option  value="' . $children->getId() . '">' . $children->getName() . '</option>';
-            }
-        }
 
-        return $result->setData(['output' => $html]);
+        $data = ['categoryId' => $categoryId];
+
+        $block = $resultPage->getLayout()
+            ->createBlock('Comerline\ConfigCar\Block\ConfigCar')
+            ->setTemplate('Comerline_ConfigCar::product/view/comparable-options.phtml')
+            ->setData('data', $data)
+            ->toHtml();
+
+        return $result->setData(['output' => $block]);
     }
 }
--- app/code/Comerline/ConfigCar/view/frontend/web/js/config-car.js
+++ app/code/Comerline/ConfigCar/view/frontend/web/js/config-car.js
@@ -5,6 +5,7 @@ define(['jquery', 'Magento_Ui/js/modal/modal', 'domReady', 'mage/cookies'], func
             $('#configcar-modal-button').html($.cookie('llantas_user_text')); // Replace button text on page load for the one we have in the cookie
             if ($.cookie('llantas_user_car')) {
                 changeModal($.cookie('llantas_user_car'), config.categoriesAttribute, '#compatible-options', null, '#car-name');
+                changeModal($.cookie('llantas_user_car'), config.categoriesAttribute, '#compatibles');
             }
         });
         let options = {
