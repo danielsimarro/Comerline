8c330e03c663dbdd187c7443783b3289a513fb36
Alberto-Privado <alberto.diciembre.29@gmail.com>
2022-02-15 10:41:04 +0100

    (feat) Config Car Module - Fixed cache problems
    - Fixed an error with cache that made navigation difficult
    - Tested and all is working as intended
    
    refs: #1vj5w3r

--- app/code/Comerline/ConfigCar/Controller/AjaxHandler/Attributes.php
+++ app/code/Comerline/ConfigCar/Controller/AjaxHandler/Attributes.php
@@ -49,36 +49,34 @@ class Attributes extends Action
         $result = $this->resultJsonFactory->create();
         $resultPage = $this->resultPageFactory->create();
         $categoryId = $this->getRequest()->getParam('frame');
-        $cookieCategory = $this->cookieManager->getCookie('llantas_user_car');
-        if ($categoryId) {
-            if ($cookieCategory === $categoryId) {
-                $variations = $this->cookieManager->getCookie('llantas_variations');
-            } else {
-                $yearCategory = $this->categoryFactory->create()->load($categoryId);
-                $yearCategoryName = $yearCategory->getName();
-                $path = explode('/', $yearCategory->getPath());
-                $brandCategoryId = $path[3];
-                $brandCategory = $this->categoryFactory->create()->load($brandCategoryId);
-                $brandCategoryName = $brandCategory->getName();
-                $modelCategoryId = $path[4];
-                $modelCategory = $this->categoryFactory->create()->load($modelCategoryId);
-                $modelCategoryName = $modelCategory->getName();
+        $variationsLocal = $this->getRequest()->getParam('variations');
+        $variations = [];
+        if ($variationsLocal) {
+            $variations = $variationsLocal;
+        } else if ($categoryId) {
+            $yearCategory = $this->categoryFactory->create()->load($categoryId);
+            $yearCategoryName = $yearCategory->getName();
+            $path = explode('/', $yearCategory->getPath());
+            $brandCategoryId = $path[3];
+            $brandCategory = $this->categoryFactory->create()->load($brandCategoryId);
+            $brandCategoryName = $brandCategory->getName();
+            $modelCategoryId = $path[4];
+            $modelCategory = $this->categoryFactory->create()->load($modelCategoryId);
+            $modelCategoryName = $modelCategory->getName();
 
-                $csvData = $this->configCarHelper->getFilteredCsvData($brandCategoryName, $modelCategoryName, $yearCategoryName);
-                $variations = [];
-                foreach ($csvData as $csv) {
-                    $diameter = $this->configCarHelper->mountOptionText($csv['diametro']) . "''";
-                    $width = $this->configCarHelper->mountOptionText($csv['ancho']) . "cm";
-                    $offset = $this->configCarHelper->mountOptionText($csv['et']) . "mm";
-                    $hub = $this->configCarHelper->mountOptionText($csv['buje']) . "mm";
-                    $variations[] = $diameter . "," . $width . "," . $offset . "," . $hub . ",";
-                }
-                $variations = json_encode($variations);
+            $csvData = $this->configCarHelper->getFilteredCsvData($brandCategoryName, $modelCategoryName, $yearCategoryName);
+            foreach ($csvData as $csv) {
+                $diameter = $this->configCarHelper->mountOptionText($csv['diametro']) . "''";
+                $width = $this->configCarHelper->mountOptionText($csv['ancho']) . "cm";
+                $offset = $this->configCarHelper->mountOptionText($csv['et']) . "mm";
+                $hub = $this->configCarHelper->mountOptionText($csv['buje']) . "mm";
+                $variations[] = $diameter . "," . $width . "," . $offset . "," . $hub . ",";
             }
-            $text = $this->cookieManager->getCookie('llantas_user_text');
-
-            $data = ['categoryId' => $categoryId, 'validVariations' => $variations, 'validText' => $text];
+            $variations = json_encode($variations);
         }
+        $text = $this->cookieManager->getCookie('llantas_user_text');
+
+        $data = ['categoryId' => $categoryId, 'validVariations' => $variations, 'validText' => $text];
 
         $block = $resultPage->getLayout()
             ->createBlock('Comerline\ConfigCar\Block\ConfigCar')
--- app/code/Comerline/ConfigCar/view/frontend/web/js/config-car.js
+++ app/code/Comerline/ConfigCar/view/frontend/web/js/config-car.js
@@ -1,17 +1,18 @@
-define(['jquery', 'Magento_Ui/js/modal/modal', 'domReady', 'mage/cookies'], function ($, modal) {
+define(['jquery', 'Magento_Ui/js/modal/modal', 'domReady', 'mage/cookies', 'Magento_Customer/js/customer-data'], function ($, modal) {
     "use strict";
     return function (config) {
         $(document).ready(function () {
-            changeModal($.cookie('llantas_user_car'), config.categoriesAttribute, '#compatible-options');
+            changeModal(null, config.categoriesAttribute, '#compatible-options', null, null, window.localStorage.getItem('llantas_variations'));
             $('#configcar-modal-button').html($.cookie('llantas_user_text')); // Replace button text on page load for the one we have in the cookie
             let options = {
                 type: 'popup', responsive: true, innerScroll: true, title: 'Configurador de coche', buttons: [{
                     text: $.mage.__('Select'), class: 'accept', click: function () {
-                        $.cookie('llantas_variations', null);
+                        window.localStorage.removeItem('llantas_variations', null);
                         $.cookie('llantas_user_car', ($('#llantas_ano option:selected').val()));
+                        $.cookie('llantas_user_year', $('#llantas_ano option:selected').text());
                         $.cookie('llantas_user_text', ($.cookie('llantas_user_brand') + ' ' + $.cookie('llantas_user_model') + ' ' + $.cookie('llantas_user_year'))); // We get car model in a cookie
-                        let variations = getVariations($('#compatible-table tbody tr').not(':first-child'));
-                        $.cookie('llantas_variations', JSON.stringify(variations));
+                        let variations = getVariations($('#compatible-options #compatible-table tbody tr').not(':first-child'));
+                        window.localStorage.setItem('llantas_variations', JSON.stringify(variations));
                         $('#configcar-modal-button').html($.cookie('llantas_user_text')); // We set the button text with the car model stored in the cookie
                         $('#feedback').append('Vehículo seleccionado correctamente'); // We add a message when the change is done
                         location.reload();
@@ -20,7 +21,7 @@ define(['jquery', 'Magento_Ui/js/modal/modal', 'domReady', 'mage/cookies'], func
                     text: $.mage.__('Delete'), class: 'delete', click: function () {
                         $.cookie('llantas_user_car', null); // We delete the cookies
                         $.cookie('llantas_user_text', 'Configurador de coche');
-                        $.cookie('llantas_variations', null);
+                        window.localStorage.removeItem('llantas_variations');
                         $.cookie('llantas_user_brand', null);
                         $.cookie('llantas_user_model', null);
                         $.cookie('llantas_user_year', null);
@@ -41,44 +42,48 @@ define(['jquery', 'Magento_Ui/js/modal/modal', 'domReady', 'mage/cookies'], func
 
         $(document).on('change', '#llantas_marca', function () {
             changeModal($('#llantas_marca').val(), config.frameAction, '#llantas_modelo', '#llantas_ano');
-            $.cookie('llantas_user_brand', $('#llantas_marca option:selected').text()); // We get car brand in a cookie
+            $.cookie('llantas_user_brand', $('#llantas_marca option:selected').text());
         });
         $(document).on('change', '#llantas_modelo', function () {
             changeModal($('#llantas_modelo').val(), config.frameAction, '#llantas_ano');
-            $.cookie('llantas_user_model', $('#llantas_modelo option:selected').text()); // We get car brand in a cookie
+            $.cookie('llantas_user_model', $('#llantas_modelo option:selected').text());
         });
         $(document).on('change', '#llantas_ano', function () {
-            changeModal($('#llantas_ano').val(), config.categoriesAttribute, '#compatible-options');
-            $.cookie('llantas_user_year', $('#llantas_ano option:selected').text()); // We get car brand in a cookie
+            if ($('#llantas_ano option:selected').text() !== "Seleccione una opción") {
+                changeModal($('#llantas_ano').val(), config.categoriesAttribute, '#compatible-options');
+            }
         });
 
-        function changeModal(idOption, block, idOutput, idExtra = null, idButton = null) {
+        function changeModal(idOption, block, idOutput, idExtra = null, idButton = null, variations = null) {
+            let param = '';
             if (idOption) {
-                let param = 'frame=' + idOption;
-                $.ajax({
-                    showLoader: true,
-                    url: block,
-                    data: param,
-                    type: "GET",
-                    dataType: 'json'
-                }).done(function (data) {
-                    $(idOutput).html(data.output);
-                    if (idExtra) {
-                        $(idExtra).empty();
-                        $(idExtra).append('<option selected="selected" value="">Seleccione una opción</option>');
-                    }
-                    if (idButton && $.cookie('llantas_user_text')) {
-                        $(idButton).html($.cookie('llantas_user_text'));
-                    }
-                });
+                param = 'frame=' + idOption;
+            } else if (variations) {
+                param = 'variations=' + variations;
             }
+            $.ajax({
+                showLoader: true,
+                url: block,
+                data: param,
+                type: "POST",
+                dataType: 'json'
+            }).done(function (data) {
+                $(idOutput).html(data.output);
+                if (idExtra) {
+                    $(idExtra).empty();
+                    $(idExtra).append('<option selected="selected" value="">Seleccione una opción</option>');
+                }
+                if (idButton && $.cookie('llantas_user_text')) {
+                    $(idButton).html($.cookie('llantas_user_text'));
+                }
+            });
         }
 
         function getVariations(variations) {
             let data = Array();
-            variations.each(function(i){
+            variations.each(function (i) {
                 data[i] = '';
-                $(this).children('th').each(function(){
+                $(this).children('th').each(function () {
                     if ($(this).text()) {
                         data[i] += $(this).text() + ",";
                     }
