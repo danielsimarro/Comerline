76c2cc4a7abfd0155a45918b91f1fdd76200d4d6
mjsoler <solergmj@gmail.com>
2022-04-27 10:29:23 +0200

    feat(observations): Add plugin for observations field checkout - Create plugin - Show observations field at checkout refs: #22k42h9

---
+++ app/code/Comerline/CheckoutObservations/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Comerline_CheckoutObservations',
+    __DIR__
+);
