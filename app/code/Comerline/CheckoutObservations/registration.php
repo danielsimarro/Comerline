d4009169ba4b8f6fc271c2ae405098c8da6a8288
mjsoler <solergmj@gmail.com>
2022-04-29 13:05:10 +0200

    feat(observations): Change plugin for observations field checkout - Delete custom plugin CheckoutObservations - Install plugin Bold Order Comment refs: #22k42h9

--- app/code/Comerline/CheckoutObservations/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    'Comerline_CheckoutObservations',
-    __DIR__
-);
