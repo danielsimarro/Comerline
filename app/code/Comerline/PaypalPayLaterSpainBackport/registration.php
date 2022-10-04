10f858a840a57e8e3aa6884987383aeba6ad8e35
Imanol Romera <imanolrrl99@gmail.com>
2022-10-04 14:10:19 +0200

    feat: Backport Paypal PayLater to Spain.
    refs: #CU-39btggx

---
+++ app/code/Comerline/PaypalPayLaterSpainBackport/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Comerline_PaypalPayLaterSpainBackport',
+    __DIR__
+);
